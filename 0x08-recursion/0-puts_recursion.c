012ff8e6453a5bc36407f3d83bc6de044b9ba96e
Billy Wellington <billierangwani@gmail.com>
2022-10-01 02:58:30 +0200

    create 0-purs_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+int _putchar(char c);
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+		/*return (0);*/
+	}
+	else
+		_putchar('\n');
+	return;
+}
