522aa87fcb54b5ec52935b68f0c0cd4758e91413
Billy Wellington <billierangwani@gmail.com>
2022-10-04 12:58:46 +0200

    Initial commit

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
-* _atoi - converts a string to an int
-* @s: the string to be changed
-*
-* Return: the converted int
+* _strcpy - Copy paste string
+*@dest: destination
+*@src: source
+*Return: dest
 */
-
-int _atoi(char *s)
+char *_strcpy(char *dest, char *src)
 {
-	int sign = 1;
-	unsigned int num = 0;
-
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
-		else if (num > 0)
-			break;
-	} while (*s++);
-
-	return (num * sign);
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
+return (dest);
 }
