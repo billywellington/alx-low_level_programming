88a54bc8ec3def7f5dd4fbce550e27b4f0d59fa9
billywellington <111053029+billywellington@users.noreply.github.com>
2022-10-06 00:44:33 +0200

    fixing betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,12 @@
  * Return: 0 - success
  */
 int main(int argc, char *argv[])
-{
-    int i;
-    for (i=0; i < argc; i++)
+{	
+	int i;
+	
+	for (i = 0; i < argc; i++)
 	{
         printf("%s\n", argv[i]);
-    }
-	return(0);
+ 	}
+	return (0);
 }
